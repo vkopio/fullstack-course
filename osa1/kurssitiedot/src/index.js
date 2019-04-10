b730cf71d4a3ccc0a20fa55c4cc89133bcf1b0cb
vkopio <ville.kopio@gmail.com>
2019-04-10 14:06:49 +0300

    T 1.2

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -5,17 +5,17 @@ const Header = (props) => (
     <h1>{props.course}</h1>
 )
 
+const Part = (props) => (
+    <p>
+        {props.part} {props.exercises}
+    </p>
+)
+
 const Content = (props) => (
     <>
-        <p>
-            {props.part1} {props.exercises1}
-        </p>
-        <p>
-            {props.part2} {props.exercises2}
-        </p>
-        <p>
-            {props.part3} {props.exercises3}
-        </p>
+        <Part part={part1} exercises={exercises1} />
+        <Part part={part2} exercises={exercises2} />
+        <Part part={part3} exercises={exercises3} />
     </>
 )
 
