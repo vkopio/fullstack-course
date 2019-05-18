23814b76186b7806b66634920a3ef168408269e9
vkopio <ville.kopio@gmail.com>
2019-05-18 15:44:38 +0300

    T 3.3

--- index.js
+++ index.js
@@ -31,6 +31,17 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 app.get('/info', (req, res) => {
     const info = `<p>Puhelinluettelossa on ${persons.length} henkilön tiedot.</p>`
     const time = `<p>${new Date()}</p>`
