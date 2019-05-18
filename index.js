887536af3bb3e3911f5b68c9dcd1078a7f27ef94
vkopio <ville.kopio@gmail.com>
2019-05-18 15:40:59 +0300

    T 3.1

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.json())
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Martti Tienari",
+        "number": "040-123456",
+        "id": 2
+    },
+    {
+        "name": "Arto Järvinen",
+        "number": "040-123456",
+        "id": 3
+    },
+    {
+        "name": "Lea Kutvonen",
+        "number": "040-123456",
+        "id": 4
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
