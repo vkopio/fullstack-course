2a97b898e407033defe743b792b9f7d5d81bb4fa
vkopio <ville.kopio@gmail.com>
2019-05-19 17:52:11 +0300

    T 3.17

--- index.js
+++ index.js
@@ -95,6 +95,21 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson.toJSON())
+        })
+        .catch(error => next(error))
+})
+
 app.delete('/api/persons/:id', (request, response) => {
     Person.findByIdAndRemove(request.params.id)
         .then(result => {
