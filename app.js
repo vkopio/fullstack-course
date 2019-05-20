7baeacd02cdb6b4793719d1f4d51f2e5003152ea
vkopio <ville.kopio@gmail.com>
2019-05-20 14:22:04 +0300

    T 4.2

---
+++ app.js
@@ -0,0 +1,16 @@
+const config = require('./utils/config')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
