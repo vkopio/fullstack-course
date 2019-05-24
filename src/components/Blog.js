3f6b6795fb1d18817979de8e71f0d7838631907d
vkopio <ville.kopio@gmail.com>
2019-05-24 11:39:55 +0300

    T 5.6

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,34 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [details, setDetails] = useState(false)
+
+  const showDetails = { display: details ? '' : 'none' }
+
+  const toggleDetails = () => {
+    setDetails(!details)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={() => toggleDetails()}>
+        {blog.title}, {blog.author}
+      </div>
+      <div style={showDetails}>
+        <p><a href={blog.url}>{blog.url}</a></p>
+        <p>{blog.likes} likes</p>
+        <p>Added by {blog.user.name}</p>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
