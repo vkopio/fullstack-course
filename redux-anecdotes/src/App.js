0100bfd7adf0c012ca4fcc89ed987d1d9ba61159
vkopio <ville.kopio@gmail.com>
2019-05-27 16:33:01 +0300

    T 6.16

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import anecdoteService from './services/anecdotes'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
-const App = () => {
+const App = (props) => {
+  useEffect(() => {
+    anecdoteService.getAll().then(anecdotes =>
+      props.initializeAnecdotes(anecdotes)
+    )
+  }, [])
+
   return (
     <div>
       <Filter />
@@ -15,4 +24,4 @@ const App = () => {
   )
 }
 
-export default App
+export default connect(null, { initializeAnecdotes })(App)
