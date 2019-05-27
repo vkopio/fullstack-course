64ada43333f772afb2e235f6a83cb6e55722f533
vkopio <ville.kopio@gmail.com>
2019-05-27 18:20:24 +0300

    T 7.1

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -1,4 +1,8 @@
 import React, { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Route, Link, Redirect, withRouter
+} from 'react-router-dom'
 
 const Menu = () => {
   const padding = {
@@ -6,9 +10,9 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link to='/' style={padding}>anecdotes</Link>
+      <Link to='/create' style={padding}>create new</Link>
+      <Link to='/about' style={padding}>about</Link>
     </div>
   )
 }
@@ -74,7 +78,7 @@ const CreateNew = (props) => {
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
         </div>
         <button>create</button>
       </form>
@@ -123,14 +127,28 @@ const App = () => {
   }
 
   return (
-    <div>
-      <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
+    <>
+      <Router>
+        <div>
+          <h1>Software anecdotes</h1>
+          <Menu />
+
+          <Route exact path="/" render={() =>
+            <AnecdoteList anecdotes={anecdotes} />
+          } />
+
+          <Route exact path="/about" render={() =>
+            <About />
+          } />
+
+          <Route exact path="/create" render={() =>
+            <CreateNew addNew={addNew} />
+          } />
+        </div>
+      </Router>
+
       <Footer />
-    </div>
+    </>
   )
 }
 
