d57b657ee62a7e23654852f3e4ce81f6a94c2132
vkopio <ville.kopio@gmail.com>
2019-06-01 18:03:31 +0300

    T 8.6

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer, UserInputError, gql } = require('apollo-server')
+const uuid = require('uuid/v1')
 
-const authors = [
+let authors = [
     {
         name: 'Robert Martin',
         id: 'afa51ab0-344d-11e9-a414-719c6709cf3e',
@@ -31,7 +32,7 @@ const authors = [
  * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
  */
 
-const books = [
+let books = [
     {
         title: 'Clean Code',
         published: 2008,
@@ -105,6 +106,15 @@ const typeDefs = gql`
         allBooks(author: String, genre: String): [Book!]!
         allAuthors: [Author!]!
     }
+
+    type Mutation {
+        addBook(
+            title: String!
+            published: Int!
+            author: String!
+            genres: [String]!
+        ): Book
+    }
 `
 
 const resolvers = {
@@ -126,9 +136,30 @@ const resolvers = {
         },
         allAuthors: () => authors,
     },
+
     Author: {
         bookCount: root => {
             return books.filter(book => book.author === root.name).length
+        },
+    },
+
+    Mutation: {
+        addBook: (root, args) => {
+            if (books.find(book => book.title === args.title)) {
+                throw new UserInputError('The book already exists', {
+                    invalidArgs: args.title,
+                })
+            }
+
+            const book = { ...args, id: uuid() }
+            books = books.concat(book)
+
+            if (!authors.some(author => author.name === args.author)) {
+                const author = { name: args.author, id: uuid() }
+                authors = authors.concat(author)
+            }
+
+            return book
         },
     },
 }
