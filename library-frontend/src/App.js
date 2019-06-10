a70aa4b6d8b0e4700973d46c28c4dd19359067cb
vkopio <ville.kopio@gmail.com>
2019-06-10 10:05:03 +0300

    T 8.24

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react'
+import { Subscription } from 'react-apollo'
+import { gql } from 'apollo-boost'
 import { useApolloClient } from 'react-apollo-hooks'
 import { useQuery, useMutation } from 'react-apollo-hooks'
 import { ALL_AUTHORS, ALL_BOOKS, ME } from './graphql/queries'
@@ -8,6 +10,20 @@ import Books from './components/Books'
 import NewBook from './components/NewBook'
 import Login from './components/Login'
 import Recommendations from './components/Recommendations'
+
+const BOOK_ADDED = gql`
+subscription {
+    bookAdded {
+        title
+        author {
+            name
+        }
+        published
+        genres
+        id
+    }
+}
+`
 
 const App = () => {
     const client = useApolloClient()
@@ -112,6 +128,15 @@ const App = () => {
                 result={allBooks}
                 me={me}
             />
+
+            <Subscription
+                subscription={BOOK_ADDED}
+                onSubscriptionData={({ subscriptionData }) => {
+                    alert(`A book named "${subscriptionData.data.bookAdded.title}" was added!`)
+                }}
+            >
+                {() => null}
+            </Subscription>
 
         </div>
     )
